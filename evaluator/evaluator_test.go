2827c722fadb6466c97506d53533c442f8f55fa3
Masahiro <sas.surfer0jonny@gmail.com>
2020-05-05 11:26:01 +0900

    interpret strings

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -308,3 +308,17 @@ let addTwo = newAdder(2);
 addTwo(2);`
 	testIntegerObject(t, testEval(input), 4)
 }
+
+func TestStringLiteral(t *testing.T) {
+	input := `"hello, world!";`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "hello, world!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
